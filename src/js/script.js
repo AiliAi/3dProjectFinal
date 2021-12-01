7c75f915b6066229803bd5d9f419e585ec381452
irina <irina.konovalova455@gmail.com>
2021-12-01 21:59:49 +0200

    front page not finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,177 +1,41 @@
 import '../styles/main.scss';
 import '../modules/nav-loading.js';
+import '../modules/frontImage.js';
+
+
 import * as bootstrap from 'bootstrap';
-
-import * as THREE from 'three'
-
-
-let camera, scene, renderer;
-let navbar = 56;
-
-let isUserInteracting = false,
-    onPointerDownMouseX = 0, onPointerDownMouseY = 0,
-    lon = 0, onPointerDownLon = 0,
-    lat = 0, onPointerDownLat = 0,
-    phi = 0, theta = 0;
-
-init();
-animate();
-
-function init() {
-
-    const container = document.getElementById('container');
-
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1100);
-
-    scene = new THREE.Scene();
-
-    const geometry = new THREE.SphereGeometry(500, 60, 40);
-    // invert the geometry on the x-axis so that all of the faces point inward
-    geometry.scale(- 1, 1, 1);
-
-    const texture = new THREE.TextureLoader().load('pictures/Karaportti-aula.jpg');
-    const material = new THREE.MeshBasicMaterial({ map: texture });
-
-    const mesh = new THREE.Mesh(geometry, material);
-
-    scene.add(mesh);
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight-navbar);
-    container.appendChild(renderer.domElement);
-
-    container.style.touchAction = 'none';
-    container.addEventListener('pointerdown', onPointerDown);
-
-    //document.addEventListener('wheel', onDocumentMouseWheel);
-
-    document.addEventListener('dragover', function (event) {
-
-        event.preventDefault();
-        event.dataTransfer.dropEffect = 'copy';
-
-    });
-
-    document.addEventListener('dragenter', function () {
-
-        document.body.style.opacity = 0.5;
-
-    });
-
-    document.addEventListener('dragleave', function () {
-
-        document.body.style.opacity = 1;
-
-    });
-
-    document.addEventListener('drop', function (event) {
-
-        event.preventDefault();
-
-        const reader = new FileReader();
-        reader.addEventListener('load', function (event) {
-
-            material.map.image.src = event.target.result;
-            material.map.needsUpdate = true;
-
-        });
-        reader.readAsDataURL(event.dataTransfer.files[0]);
-
-        document.body.style.opacity = 1;
-
-    });
-
-    //
-
-    window.addEventListener('resize', onWindowResize);
-
-}
-
-function onWindowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight-navbar);
-
-}
-
-function onPointerDown(event) {
-
-    if (event.isPrimary === false) return;
-
-    isUserInteracting = true;
-
-    onPointerDownMouseX = event.clientX;
-    onPointerDownMouseY = event.clientY;
-
-    onPointerDownLon = lon;
-    onPointerDownLat = lat;
-
-    document.addEventListener('pointermove', onPointerMove);
-    document.addEventListener('pointerup', onPointerUp);
-
-}
-
-function onPointerMove(event) {
-
-    if (event.isPrimary === false) return;
-
-    lon = (onPointerDownMouseX - event.clientX) * 0.1 + onPointerDownLon;
-    lat = (event.clientY - onPointerDownMouseY) * 0.1 + onPointerDownLat;
-
-}
-
-function onPointerUp() {
-
-    if (event.isPrimary === false) return;
-
-    isUserInteracting = false;
-
-    document.removeEventListener('pointermove', onPointerMove);
-    document.removeEventListener('pointerup', onPointerUp);
-
-}
-
-/*function onDocumentMouseWheel(event) {
-
-    const fov = camera.fov + event.deltaY * 0.05;
-
-    camera.fov = THREE.MathUtils.clamp(fov, 10, 75);
-
-    camera.updateProjectionMatrix();
-
-}*/
-
-function animate() {
-
-    requestAnimationFrame(animate);
-    update();
-
-}
-
-function update() {
-
-    if (isUserInteracting === false) {
-
-        lon += 0.1;
-
-    }
-
-    lat = Math.max(- 85, Math.min(85, lat));
-    phi = THREE.MathUtils.degToRad(90 - lat);
-    theta = THREE.MathUtils.degToRad(lon);
-
-    const x = 500 * Math.sin(phi) * Math.cos(theta);
-    const y = 500 * Math.cos(phi);
-    const z = 500 * Math.sin(phi) * Math.sin(theta);
-
-    camera.lookAt(x, y, z);
-
-    renderer.render(scene, camera);
-}
-
+import 'bootstrap-icons/font/bootstrap-icons.scss';
+
+const left = document.getElementById('left');
+const btnLeft = document.getElementById('btn-left');
+const right = document.getElementById('right');
+const btnRight = document.getElementById('btn-right');
+const homeText = document.getElementById('heading-text');
+const mainBtn = document.getElementById('main-btn');
+
+left.addEventListener('mouseenter', e => {
+  btnLeft.className = 'btn btn-info'
+});
+
+left.addEventListener('mouseleave', e => {
+  btnLeft.className = 'btn btn-outline-info'
+});
+
+right.addEventListener('mouseenter', e => {
+  btnRight.className = 'btn btn-light'
+});
+
+right.addEventListener('mouseleave', e => {
+  btnRight.className = 'btn btn-outline-light'
+});
+
+homeText.addEventListener('mouseenter', e => {
+  mainBtn.className = 'btn btn-info'
+});
+
+homeText.addEventListener('mouseleave', e => {
+  mainBtn.className = 'btn btn-outline-info'
+});
 
 
 
