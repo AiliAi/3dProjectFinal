88df232a3d4d26ff370303ee9d017adb7a1157ec
AiliAi <49479422+AiliAi@users.noreply.github.com>
2021-12-06 12:17:58 +0200

    Update webpack.dev.js

--- bundler/webpack.dev.js
+++ bundler/webpack.dev.js
@@ -1,4 +1,3 @@
-const path = require('path')
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
 const ip = require('internal-ip')
@@ -12,32 +11,23 @@ const infoColor = (_message) =>
 module.exports = merge(
     commonConfiguration,
     {
-        stats: 'errors-warnings',
         mode: 'development',
         devServer:
         {
-            host: 'local-ip',
+            host: '0.0.0.0',
             port: portFinderSync.getPort(8080),
+            contentBase: './dist',
+            watchContentBase: true,
             open: true,
             https: false,
-            allowedHosts: 'all',
-            hot: false,
-            watchFiles: ['src/**', 'static/**'],
-            static:
+            useLocalIp: true,
+            disableHostCheck: true,
+            overlay: true,
+            noInfo: true,
+            after: function(app, server, compiler)
             {
-                watch: true,
-                directory: path.join(__dirname, '../static')
-            },
-            client:
-            {
-                logging: 'none',
-                overlay: true,
-                progress: false
-            },
-            onAfterSetupMiddleware: function(devServer)
-            {
-                const port = devServer.options.port
-                const https = devServer.options.https ? 's' : ''
+                const port = server.options.port
+                const https = server.options.https ? 's' : ''
                 const localIp = ip.v4.sync()
                 const domain1 = `http${https}://${localIp}:${port}`
                 const domain2 = `http${https}://localhost:${port}`
@@ -45,5 +35,5 @@ module.exports = merge(
                 console.log(`Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(domain2)}`)
             }
         }
-    }
+    },
 )
